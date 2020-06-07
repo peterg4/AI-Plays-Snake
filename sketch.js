cc1b2018d25fa5217833dd29d7c2b760069b4d36
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-06-07 09:07:35 -0400

    working game w/ restart and reverse guarding

--- sketch.js
+++ sketch.js
@@ -1,13 +1,59 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   w = floor(width / rez);
   h = floor(height / rez);
-  frameRate(24);
+  frameRate(10);
+  var button = createButton('Restart');
+  button.mousePressed(restart);
   snake = new Snake();
   foodLocation();
 }
 
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
 
-function draw(){
-  background(240);
+}
+
+function keyPressed() {
+  switch(keyCode) {
+    case LEFT_ARROW: snake.setVelocity(-1, 0); break;
+    case RIGHT_ARROW: snake.setVelocity(1, 0); break;
+    case UP_ARROW: snake.setVelocity(0, -1); break;
+    case DOWN_ARROW: snake.setVelocity(0, 1); break;
+  }
+}
+
+function restart() {
+  snake = new Snake();
+  foodLocation()
+  background(220);
+  loop();
+}
+
+function draw() {
+  scale(rez);
+  background(220);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
 }
--- snake.js
+++ snake.js
@@ -1,22 +1,65 @@
 class Snake {
+  
   constructor() {
-    this.body = [];
+  	this.body = [];
     this.body[0] = createVector(floor(w/2), floor(h/2));
-    this.len = 1;
-    this.xVelocity;
-    this.yVelocity;
+    this.xVelocity = 0;
+    this.yVelocity = 0;
+    this.len = 0;
   }
-
+  
   setVelocity(x, y) {
-    this.xVelocity = x;
-    this.yVelocity = y;
+    if(Math.abs(x - this.xVelocity) != 2 &&  Math.abs(y - this.yVelocity) != 2) {
+      this.xVelocity = x;
+      this.yVelocity = y;
+    }
   }
-
+  
   update() {
-
+  	let head = this.body[this.body.length-1].copy();
+    this.body.shift();
+    head.x += this.xVelocity;
+    head.y += this.yVelocity;
+    this.body.push(head);
   }
-
+  
+  grow() {
+  	let head = this.body[this.body.length-1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+  
+  endGame() {
+  	let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if(x > w-1 || x < 0 || y > h-1 || y < 0) {
+       return true;
+    }
+    for(let i = 0; i < this.body.length-1; i++) {
+    	let part = this.body[i];
+      if(part.x == x && part.y == y) {
+      	return true;
+      }
+    }
+    return false;
+  }
+  
+  eat(pos) {
+  	let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if(x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+  
   show() {
-
+  	for(let i = 0; i < this.body.length; i++) {
+    	fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1)
+    }
   }
+
 }
